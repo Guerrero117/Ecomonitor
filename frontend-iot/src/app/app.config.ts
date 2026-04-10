1382d1770224eb873390dc12b5b47ab83961333a
Mahatmaemma <jauregui.mahatma.cbtis37@hotmail.com>
2026-04-09 17:31:39 -0700

    Ajuste final: SPA estática y URLs dinámicas para Raspberry

--- frontend-iot/src/app/app.config.ts
+++ frontend-iot/src/app/app.config.ts
@@ -1,22 +1,20 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http'; 
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
-// CORRECCIÓN: Import con "I" Mayúscula para que coincida con tu carpeta física
+// Importamos el interceptor que ya tienes
 import { authInterceptor } from './core/Interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), 
-    provideClientHydration(withEventReplay()),
     provideCharts(withDefaultRegisterables()),
     provideHttpClient(
       withFetch(),
-      withInterceptors([authInterceptor]) // Esto activa el envío del token
+      withInterceptors([authInterceptor]) 
     ),
   ]
 };
--- frontend-iot/src/environments/environment.ts
+++ frontend-iot/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // Detecta la IP actual automáticamente para el API en el puerto 5126
+  // Detecta automáticamente la IP de la laptop de tu compañero
   apiUrl: `http://${typeof window !== 'undefined' ? window.location.hostname : 'localhost'}:5126/api` 
 };
